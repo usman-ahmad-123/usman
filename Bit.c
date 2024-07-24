be760ede54ac548300fee0d66d40e59ba5eaae4d
usman-ahmad-123 <mdusmanahmad8@gmail.com>
2024-07-24 23:10:22 +0530

    Add new file

---
+++ Bit.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+  int n;
+  scanf("%d",&n);  // 1<=n<=150
+  for(int i=1;i<=n;i++){
+    int r;
+    char ch=(char)r;
+    scanf("%c",&ch);
+    if(r==43){
+      printf("%c",r);
+    }
+    else{
+      printf("%c",r);
+    }
+    printf("%d\n",n);
+  }
+  
+  return 0;
+}
